31635d0fbfb7543578b0eb81d7b174446def72b7
vishal kumar <vishalvarnwal25@outlook.com>
2020-04-28 04:12:42 +0530

    header name changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Header = () =>(
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>HomePage</NavLink>
-        <NavLink to="/create" activeClassName="is-active">CreatePage</NavLink>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">CreateExpense</NavLink>
         <NavLink to="/help" activeClassName="is-active">HelpPage</NavLink>
     </header>
 );
